9a913395f37d6437a33dd1e56e77434a7dac4c30
Mateus Abelli <mateusabelli@gmail.com>
2022-12-26 22:06:37 -0300

    Fix "+" symbol appearing on queries

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (args.length === 0) {
   console.log("You must execute this script with a search query!");
   return;
 }
-const query = encodeURIComponent(args.join("+").toString());
+const query = encodeURIComponent(args.join(" ").toString());
 
 //TODO: Check the users platform to execute the command accordingly
 exec(`explorer "https://www.youtube.com/results?search_query=${query}"`);
